592e1cda03ae46a2a95f90e5d40ecdab8346d08f
Predictive Works <info@predictiveworks.eu>
2022-06-07 18:50:12 +0200

    Dragino project updated.

--- sensor-dragino/src/main/scala/de/kp/works/beats/sensor/dragino/DoStack.scala
+++ sensor-dragino/src/main/scala/de/kp/works/beats/sensor/dragino/DoStack.scala
@@ -20,7 +20,6 @@ package de.kp.works.beats.sensor.dragino
  */
 
 import ch.qos.logback.classic.Logger
-import com.google.gson.JsonParser
 import de.kp.works.beats.sensor.dragino.enums.DoProducts.{LDDS04, LDDS20, LDDS45}
 import de.kp.works.beats.sensor.thingsstack.Consumer
 import org.eclipse.paho.client.mqttv3.MqttMessage
@@ -42,61 +41,9 @@ class DoStack(options: DoOptions) extends Consumer[DoConf](options.toStack) with
 
     try {
 
-      val payload = new String(mqttMessage.getPayload)
-      val json = JsonParser.parseString(payload)
+      val (deviceId, sensorReadings) = unpack(mqttMessage)
       /*
-       * Extract uplink message and associated
-       * decoded payload
-       */
-      val messageObj = json.getAsJsonObject
-      /*
-       * STEP #1: Extract the unique TTN device identifier,
-       * which is also used to uniquely identify the sensor.
-       *
-       * {
-       *  "end_device_ids" : {
-       *    "device_id" : "dev1",                    // Device ID
-       *    "application_ids" : {
-       *      "application_id" : "app1"              // Application ID
-       *    },
-       *    "dev_eui" : "0004A30B001C0530",          // DevEUI of the end device
-       *    "join_eui" : "800000000000000C",         // JoinEUI of the end device (also known as AppEUI in LoRaWAN versions below 1.1)
-       *    "dev_addr" : "00BCB929"                  // Device address known by the Network Server
-       * },
-       *
-       * ...
-       */
-      val endDeviceIds = messageObj
-        .get(TTN_END_DEVICE_IDS).getAsJsonObject
-
-      val deviceId = endDeviceIds
-        .get(TTN_DEVICE_ID).getAsString
-      /*
-       * STEP #2: Extract the decoded payload from the
-       * provided TTN v3 uplink message
-       */
-      val uplinkMessage = messageObj.get(TTN_UPLINK_MESSAGE).getAsJsonObject
-      var sensorReadings = uplinkMessage.get(TTN_DECODED_PAYLOAD).getAsJsonObject
-      /*
-       * The current implementation of the decoded payload
-       * has the following format:
-       *
-       * {
-       *    data: ...
-       * }
-       *
-       * SensorBeat is based on a common {key, value} format
-       */
-      if (sensorReadings.has("data")) {
-        /*
-         * Flatten the sensor readings
-         */
-        val data = sensorReadings.remove("data").getAsJsonObject
-        sensorReadings = data
-      }
-      /*
-        * STEP #3: Convert decoded sensors that refer
-        * to textual values
+        * Convert decoded sensors that refer to textual values
         */
       val product = options.getProduct
       product match {
